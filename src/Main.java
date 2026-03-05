a5cc01e91ba70e82b86efde70509ea6b3ff8f91c
jaslyn-j <jaslynjacob026@gmail.com>
2026-03-05 14:27:58 +0530

    UC1: App entry

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+/*
+@author: Jaslyn Jacob
+@version: 1.0;
+ */
+
+public class Main{
+  public static void main(String[] args){
+    System.out.println("Welcome to Hotel Management System");
+    System.out.println("System initialized successfully");
+  }
+}
