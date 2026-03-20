b1f40c9ce5d3a8b9ca805320ab3358c5b851c748
jaslyn-j <jaslynjacob026@gmail.com>
2026-03-20 14:32:40 +0530

    uc5

--- src/Main.java
+++ src/Main.java
@@ -1,86 +1,60 @@
 import java.util.*;
 /*
 @author: Jaslyn Jacob
-@version: 3.0;
+@version: 5.0;
  */
-abstract class Room4 {
 
-  String type;
-  double price;
+class Reservation5 {
 
-  Room4(String type, double price) {
-    this.type = type;
-    this.price = price;
+  String guestName;
+  String roomType;
+
+  Reservation5(String guestName, String roomType) {
+    this.guestName = guestName;
+    this.roomType = roomType;
   }
 
   void display() {
-    System.out.println(type + " Price: " + price);
+    System.out.println("Processing booking for Guest: "+guestName + ", Room Type: "+roomType);
   }
 }
 
-class SingleRoom4 extends Room4 {
-  SingleRoom4() {
-    super("Single", 1000);
-  }
-}
+class BookingQueue5 {
 
-class DoubleRoom4 extends Room4 {
-  DoubleRoom4() {
-    super("Double", 2000);
-  }
-}
+  Queue<Reservation5> queue;
 
-class SuiteRoom4 extends Room4 {
-  SuiteRoom4() {
-    super("Suite", 5000);
-  }
-}
-
-class RoomInventory4 {
-
-  HashMap<String, Integer> inventory;
-
-  RoomInventory4() {
-    inventory = new HashMap<>();
-
-    inventory.put("Single", 5);
-    inventory.put("Double", 0);
-    inventory.put("Suite", 2);
+  BookingQueue5() {
+    queue = new LinkedList<>();
   }
 
-  int getAvailability(String type) {
-    return inventory.get(type);
+  void addRequest(Reservation5 r) {
+    queue.add(r);
+  }
+
+  void showRequests() {
+    for (Reservation5 r : queue) {
+      r.display();
+    }
   }
 }
 
 public class Main{
 
-  static String version = "Hotel Booking System v4.0";
+  static String version = "Hotel Booking System v5.0";
 
   public static void main(String[] args) {
 
     System.out.println("Book My Stay");
     System.out.println(version);
 
-    RoomInventory4 inv = new RoomInventory4();
+    BookingQueue5 q = new BookingQueue5();
 
-    Room4 r1 = new SingleRoom4();
-    Room4 r2 = new DoubleRoom4();
-    Room4 r3 = new SuiteRoom4();
+    q.addRequest(new Reservation5("A", "Single"));
+    q.addRequest(new Reservation5("B", "Double"));
+    q.addRequest(new Reservation5("C", "Suite"));
 
-    if (inv.getAvailability("Single") > 0) {
-      r1.display();
-      System.out.println("Available: " + inv.getAvailability("Single"));
-    }
+    System.out.println("Booking Requests in Order:");
 
-    if (inv.getAvailability("Double") > 0) {
-      r2.display();
-      System.out.println("Available: " + inv.getAvailability("Double"));
-    }
-
-    if (inv.getAvailability("Suite") > 0) {
-      r3.display();
-      System.out.println("Available: " + inv.getAvailability("Suite"));
-    }
+    q.showRequests();
   }
 }
